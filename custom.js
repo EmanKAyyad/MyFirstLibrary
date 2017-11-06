26bec9ccf10189f0d3eb600e8e1961ef95d93e14
Eman Khalifa <eman_khalifa_ayyad@hotmail.com>
2017-11-06 23:01:35 +0100

    Add a features to create multiple instances with their own methods. thus controlling each one on it's own

--- custom.js
+++ custom.js
@@ -1,2 +1,7 @@
 var mybg  = B$('blue',600,400);
+var secondBg = B$("black" , 50 , 50 );
+var thirdBg = B$("yellow",150,100);
+
+secondBg.createBG().applyColor().changeColor("red");
 mybg.createBG().applyColor().changeColor("yellow");
+thirdBg.createBG().applyColor().changeColor("black");
--- myLib.js
+++ myLib.js
@@ -1,6 +1,6 @@
 (function ( global ){
     //this library is to create an object with it's method's to create and edit dom element.
-    
+    global.bgCounter = 0;
     var availableColors = ['red','green','blue','yellow','black'];
     
     var background = function(color,bgWidth,bgHeight){
@@ -17,34 +17,39 @@
     // methods added to the prototype of the object created for better and faster performance.
     
     BG.prototype = {
-        
         createBG : function(){
             
             var newBG = document.createElement("div");
             newBG.style.width = this.width +"px";
             newBG.style.height = this.height + "px";
-            newBG.setAttribute("class","Bg-background");
+            newBG.className = "B$";
+            newBG.setAttribute("id", "Bg-background-" + global.bgCounter);
             document.getElementsByTagName("body")[0].appendChild(newBG);
+            this.counter = global.bgCounter;
+            global.bgCounter++;
             // return this added to return the processed object for further edits.
+            
             return this;
+
         },
         applyColor : function(){
-            
+            var self = this;
             if(availableColors.indexOf(this.color) === -1){
                 throw "Color not available";
             
             }else{
-                document.getElementsByClassName("Bg-background")[0].style.backgroundColor = this.color;
+                document.getElementById("Bg-background-" + self.counter).style.backgroundColor = this.color;
             }
             return this;
         },
         changeColor : function(newColor){
             
+            var self = this;
             if(availableColors.indexOf(newColor) === -1){
                 throw "Color not available";
             }else{
                 setInterval(function(){
-                    document.getElementsByClassName("Bg-background")[0].style.backgroundColor = newColor;
+                document.getElementById("Bg-background-" + self.counter).style.backgroundColor = newColor;
                 },500);
             }
             return this;
